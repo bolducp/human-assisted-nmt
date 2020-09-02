41398709f82d300e140a775ffd82c84ba6e9a718
Paige Finkelstein <paigelfink@gmail.com>
2020-09-02 12:21:57 -0400

    add human effort vs accuracy score graphs

--- hnmt/feedback_requester/experiments/graphs.py
+++ hnmt/feedback_requester/experiments/graphs.py
@@ -25,12 +25,15 @@ def plot_score_and_acc_over_docs(
 
     save_plot_image(num_docs, averages['ksmr'], 'KSMR', run_name)
     save_plot_image(num_docs, averages['orig_nmt_out_bleu'], 'Original BLEU', run_name)
-    save_plot_image(num_docs, averages['orig_nmt_out_chrf'], 'Original Chrf', run_name)
+    save_plot_image(num_docs, averages['orig_nmt_out_chrf'], 'Original ChrF', run_name)
     save_plot_image(num_docs, averages['post_feedback_bleu'], 'Post Feedback BLEU', run_name)
     save_plot_image(num_docs, averages['post_feedback_chrf'], 'Post Feedback ChrF', run_name)
     save_plot_image(num_docs, averages['percent_sent_requested'], 'Percent Sents Requested', run_name)
     save_plot_image(num_docs, bleu_improvement_avg, 'Bleu Improvement', run_name)
-    save_plot_image(num_docs, chrf_improvement_avg, 'Chrf Improvement', run_name)
+    save_plot_image(num_docs, chrf_improvement_avg, 'ChrF Improvement', run_name)
+
+    save_plot_map_ksmr_against_score(stats['ksmr'], stats['post_feedback_bleu'], run_name, 'BLEU')
+    save_plot_map_ksmr_against_score(stats['ksmr'], stats['post_feedback_chrf'], run_name, 'ChrF')
 
 
 def save_plot_image(
@@ -88,6 +91,23 @@ def calculate_score_improvement_averages(
     return [post_feedback_ave - orig_avg
             for post_feedback_ave, orig_avg
             in zip(post_feedback_score_avgs, original_score_avgs)]
+
+
+def save_plot_map_ksmr_against_score(
+    ksmr_scores: List[int],
+    eval_scores: List[float],
+    run_name: str,
+    title: str
+):
+    ksmr_values, scores = zip(*sorted(zip(ksmr_scores, eval_scores)))
+
+    plt.plot(ksmr_values, scores, 'g', label='No updates')
+    plt.title('{} Scores Across KSMR'.format(title))
+    plt.xlabel('KSMR (human effort)')
+    plt.ylabel(title)
+    plt.legend()
+    plt.savefig(current_dir + '/plots/{}/{}_v_KSMR.png'.format(run_name, title))
+    plt.close()
 
 
 if __name__ == "__main__":
